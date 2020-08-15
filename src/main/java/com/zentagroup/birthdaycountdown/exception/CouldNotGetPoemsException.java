a8b942508e87705541c3b523cb8e1c5afc9c39c0
Ariana Gonzalez <ariana.sgm5@gmail.com>
2020-08-15 11:46:50 -0400

    Custom exceptions and global constants

---
+++ src/main/java/com/zentagroup/birthdaycountdown/exception/CouldNotGetPoemsException.java
@@ -0,0 +1,12 @@
+package com.zentagroup.birthdaycountdown.exception;
+
+/**
+ * Ariana González
+ * ariana.sgm5@gmail.com
+ */
+
+public class CouldNotGetPoemsException extends Exception {
+    public CouldNotGetPoemsException(String message){
+        super(message);
+    }
+}
---
+++ src/main/java/com/zentagroup/birthdaycountdown/exception/InvalidBirthDateException.java
@@ -0,0 +1,12 @@
+package com.zentagroup.birthdaycountdown.exception;
+
+/**
+ * Ariana González
+ * ariana.sgm5@gmail.com
+ */
+
+public class InvalidBirthDateException extends Exception{
+    public InvalidBirthDateException(String message){
+        super(message);
+    }
+}
