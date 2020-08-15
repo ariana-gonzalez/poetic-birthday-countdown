b9570f327bc6da3922bb48fad2e460f604b1c1c8
Ariana Gonzalez <ariana.sgm5@gmail.com>
2020-08-15 11:33:46 -0400

    First commit

---
+++ src/main/java/com/zentagroup/birthdaycountdown/BirthdayCountdownApplication.java
@@ -0,0 +1,13 @@
+package com.zentagroup.birthdaycountdown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirthdayCountdownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirthdayCountdownApplication.class, args);
+	}
+
+}
